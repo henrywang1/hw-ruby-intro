db5e1f5d4ccb2950e62b83b0e9335fbb1519f897
“henrywang1” <wanghy917@gmail.com>
2020-09-06 04:45:30 +0000

    csce-606-hw1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,63 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.size == 0
+    0
+  elsif arr.size == 1
+    arr[0]
+  else
+    max_2 = arr.max(2)
+    max_2[0] + max_2[1]
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.size <= 1
+    false
+  end
+  dict = {}
+  arr.each_with_index do |num, i|
+    if dict[n - num]
+      return true
+    end
+    dict[num] = true
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, %s" % [name]
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s.length == 0 || s[0] !~ /[[:alpha:]]/ || ["a", "e", "i", "o", "u"].include?(s[0].downcase)
+    return false
+  end
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  (s =~ /^(0|(1|0)*1+(1|0)*00)$/) != nil
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if price <= 0 || isbn.empty?
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+  def price_as_string
+    "$%.2f" % @price
+  end
+  attr_accessor :isbn, :price
 end
